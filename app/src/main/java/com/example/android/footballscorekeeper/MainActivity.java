1e769d694e121ab04a96f81da67a845e0521baaa
Jeff Palutke <jeffpalutke@gmail.com>
2018-05-14 17:22:09 -0500

    Ready for submission

--- app/src/main/java/com/example/android/footballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/footballscorekeeper/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.footballscorekeeper;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    int resetButtonPressCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         displayForTeamA();
         displayForTeamB();
+    }
+
+    /**
+     * Score Reset button onClick handler
+     * if first click, increment click count and invite a second click to confirm action.
+     * if second click, reset click count and scores
+     */
+    public void resetScores_onClick(View view) {
+        if (resetButtonPressCount == 0) {
+            resetButtonPressCount++;
+            Snackbar.make(view, R.string.reset_twice_prompt,
+                    Snackbar.LENGTH_LONG).
+                    setAction("Action", null).show();
+        } else {
+            scoreTeamA = 0;
+            scoreTeamB = 0;
+            // when a score is displayed, the Reset click count is set to 0 automatically
+            displayForTeamA();
+            displayForTeamB();
+        }
     }
 
     /**
@@ -27,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the team A fieldgoal button is clicked.
+     * This method is called when the team A field goal button is clicked.
      */
     public void teamAFieldGoal(View view) {
         scoreTeamA = scoreTeamA + 3;
@@ -46,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the team A extra point button is clicked.
      */
     public void teamAExtraPoint(View view) {
-        scoreTeamA = scoreTeamA+ 1;
+        scoreTeamA = scoreTeamA + 1;
         displayForTeamA();
     }
 
@@ -59,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the team B fieldgoal button is clicked.
+     * This method is called when the team B field goal button is clicked.
      */
     public void teamBFieldGoal(View view) {
         scoreTeamB = scoreTeamB + 3;
@@ -78,18 +100,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the team B extra point button is clicked.
      */
     public void teamBExtraPoint(View view) {
-        scoreTeamB = scoreTeamB+ 1;
-        displayForTeamB();
-    }
-
-
-    /**
-     * This method is called when the RFSET button is clicked.
-     */
-    public void resetScores(View view) {
-        scoreTeamB = 0;
-        scoreTeamA = 0;
-        displayForTeamA();
+        scoreTeamB = scoreTeamB + 1;
         displayForTeamB();
     }
 
@@ -97,7 +108,8 @@ public class MainActivity extends AppCompatActivity {
      * Displays the score for Team A.
      */
     public void displayForTeamA() {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        resetButtonPressCount = 0;
+        TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(scoreTeamA));
     }
 
@@ -105,7 +117,8 @@ public class MainActivity extends AppCompatActivity {
      * Displays the score for Team B.
      */
     public void displayForTeamB() {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        resetButtonPressCount = 0;
+        TextView scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(scoreTeamB));
     }
 }
